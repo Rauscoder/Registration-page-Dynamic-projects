112f5d6b088b867f3de9163eaf96d10e5c1d3d05
Rauscoder <raushanrajsharma724@gmail.com>
2023-06-03 01:47:36 +0530

    Read All data

--- BookingAppoint.js
+++ BookingAppoint.js
@@ -20,15 +20,26 @@ function userInput(e){
           Time:time.value,
           }
   
-  axios.post("https://crudcrud.com/api/7ab884dcdf3944a19bd0b4acf2ec026c/AppointmentData",userDetails)
-  .then((response)=>{
-    showUserInput(response.data); 
-  }).catch((err)=>{
-    document.body.innerHTML="<h1>please make correct apis</h1>";
-  });
+ axios.post("https://crudcrud.com/api/7ab884dcdf3944a19bd0b4acf2ec026c/AppointmentData",userDetails)
+ .then((response)=>{
+    showUserInput(userDetails); 
+ }).catch((err)=>{
+   document.body.innerHTML="<h1>please make correct apis</h1>";
+ });
 }
   
 }
+window.addEventListener("DOMContentLoaded",()=>{
+  axios.get("https://crudcrud.com/api/7ab884dcdf3944a19bd0b4acf2ec026c/AppointmentData")
+  .then((response)=>{
+    
+    for(let i=0;i<response.data.length;i++){
+      showUserInput(response.data[i]);
+    }
+  }).catch((err)=>{
+    alert(err);
+  });
+});
 function showUserInput(obj){
     let li=document.createElement('li');
 li.appendChild(document.createTextNode(`${obj.Name}-${obj.Email}-${obj.Phone}`));
