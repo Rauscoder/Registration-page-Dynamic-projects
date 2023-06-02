d99a9a2a9fa2da32a559c56f67d403c5a5b9108f
Rauscoder <raushanrajsharma724@gmail.com>
2023-06-02 22:54:12 +0530

    first commit

---
+++ BookingAppoint.js
@@ -0,0 +1,48 @@
+let forms=document.getElementById('my-forms');
+forms.addEventListener('submit',userInput);
+let names=document.getElementById('name');
+let email=document.getElementById('mail');
+let phone=document.getElementById('phone');
+let date=document.querySelector('.datee');
+let time=document.querySelector('.timee');
+let ulist=document.getElementById("list-user");
+function userInput(e){
+   e.preventDefault();
+   if(names===''||email===''||phone==''){
+       alert("please enter all fields values");
+  }
+  else{
+    const userDetails={
+          Name:names.value,
+          Email:email.value,
+          Phone:phone.value,
+          dates:date.value,
+          Time:time.value,
+          }
+  
+  axios.post("https://crudcrud.com/api/7ab884dcdf3944a19bd0b4acf2ec026c/AppointmentData",userDetails)
+  .then((response)=>{
+    showUserInput(response.data); 
+  }).catch((err)=>{
+    document.body.innerHTML="<h1>please make correct apis</h1>";
+  });
+}
+  
+}
+function showUserInput(obj){
+    let li=document.createElement('li');
+li.appendChild(document.createTextNode(`${obj.Name}-${obj.Email}-${obj.Phone}`));
+    let btn=document.createElement('button');
+    
+    btn.id='btn1';
+    btn.className="btn btn-danger btn-sm float-right delete ml-2";
+    btn.appendChild(document.createTextNode('Delete'));
+    li.appendChild(btn);
+    
+    let btn2=document.createElement('button');
+    btn2.id='btn2';
+    btn2.className="btn btn-sm float-right btn-danger mr-2";
+    btn2.appendChild(document.createTextNode('Edit'));
+    li.appendChild(btn2);
+    ulist.appendChild(li);
+}
